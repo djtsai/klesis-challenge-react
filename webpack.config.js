b14f872273f3edebaf3d93e80deba6d3880d17b8
Daniel Tsai <danieltsai.developer@gmail.com>
2017-06-03 16:17:37 -0700

    Add hello world page

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, 'public/dist');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
     entry: APP_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
-        publicPath: '/public/',
+        publicPath: '/dist/',
         filename: 'bundle.js'
     }
 };
