aae7695e9d5a2992643f331bc1e9037d3b61d0fa
Daniel Tsai <danieltsai.developer@gmail.com>
2017-06-04 00:38:17 -0700

    Add basic routes for login and registration

--- src/containers/KlesisChallenge.jsx
+++ src/containers/KlesisChallenge.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
+import Registration from './Registration'
+import Home from './Home'
 
 class KlesisChallenge extends React.Component {
     render() {
         return (
-            <div>Hello World</div>
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/registration" component={Registration}/>
+                <Route path="/" component={Home}/>
+            </Switch>
         )
     }
 }
