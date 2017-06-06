ead31e3c44c85cfbd79bb5abb8a14ff046bfc8af
Daniel Tsai <danieltsai.developer@gmail.com>
2017-06-06 00:56:13 -0700

    Add temporary header for login page

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PageHeader from 'react-bootstrap/lib/PageHeader'
+
+import './index.scss'
 
 class Login extends React.Component {
     render() {
         return (
-            <div>Login</div>
+            <div className="login-container">
+                <PageHeader className="login-header">App is coming soon!</PageHeader>
+            </div>
         )
     }
 }
