c4d2cafbfba8d3d400f458cc3631b0d8424252fe
Daniel Tsai <danieltsai.developer@gmail.com>
2017-06-06 00:26:30 -0700

    Add notifications through toast

--- src/containers/KlesisChallenge.jsx
+++ src/containers/KlesisChallenge.jsx
@@ -1,19 +1,70 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
+import Notifications, { notify } from 'react-notify-toast'
 import Login from './Login'
 import Registration from './Registration'
 import Home from './Home'
 
+function mapStateToProps(state) {
+    return {
+        redirect: state.redirect,
+        toast: state.toast
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    const actions = {}
+
+    return bindActionCreators(actions, dispatch)
+}
+
 class KlesisChallenge extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.show = notify.createShowQueue()
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.toast.show) {
+            this.show(nextProps.toast.message, nextProps.toast.type, 3000, {})
+        }
+
+        if (nextProps.redirect !== '') {
+            this.props.history.push(nextProps.redirect)
+        }
+    }
+
     render() {
         return (
-            <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/registration" component={Registration}/>
-                <Route path="/" component={Home}/>
-            </Switch>
+            <div className="klesis-challenge-container">
+                <Notifications/>
+                <Switch>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/registration" component={Registration}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </div>
         )
     }
 }
 
-export default KlesisChallenge
+KlesisChallenge.propTypes = {
+    redirect: PropTypes.string.isRequired,
+    toast: PropTypes.shape({
+        show: PropTypes.bool.isRequired,
+        message: PropTypes.string.isRequired,
+        type: PropTypes.oneOf([
+            'success',
+            'warning',
+            'error',
+            ''
+        ]).isRequired
+    }).isRequired,
+    history: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(KlesisChallenge)
