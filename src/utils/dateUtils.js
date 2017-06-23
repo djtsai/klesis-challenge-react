651ce692c9f5348319c0103f0a19bbeb7290c0fc
Daniel Tsai <danieltsai.developer@gmail.com>
2017-06-22 21:20:53 -0700

    Update timestamp conversion

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,5 +1,5 @@
 export function utcTimestampToDate(timestamp) {
-    const date = new Date(timestamp)
+    const date = new Date(timestamp * 1000)
 
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
 }
